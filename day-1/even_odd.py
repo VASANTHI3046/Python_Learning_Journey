c648e2e6f1f2c82a2e3baf8433b968e31d1ce112
vasanthi yadala <yadhalavasanthi@gmail.com>
2026-05-28 19:32:24 +0530

    Add files via upload
    completed uploading of day-1 programs

---
+++ day-1/even_odd.py
@@ -0,0 +1,14 @@
+x = 88
+
+if x % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+
+
+for x in range(10):
+    if x % 2 == 0:
+        print("Even Number")
+    else:
+        print("Odd Number")
---
+++ day-1/even_odd_for.py
@@ -0,0 +1,5 @@
+for x in range(30):
+    if x % 2 == 0:
+        print(f"{x} is an Even Number")
+    else:
+        print(f"{x} is an odd Number")
